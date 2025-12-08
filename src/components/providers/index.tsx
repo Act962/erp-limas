15fc1574ef05fffc848f99ae3effe0d278469560
Arthur Fabrícyo <arthur.fabricyo@gmail.com>
2025-12-08 17:38:29 -0300

    feat: Add storefront product catalog, new UI components, and application providers.

--- src/components/providers/index.tsx
+++ src/components/providers/index.tsx
@@ -3,11 +3,14 @@
 import { QueryClientProvider } from "@tanstack/react-query";
 import { createQueryClient } from "@/lib/query/client";
 import { useState } from "react";
+import { NuqsAdapter } from "nuqs/adapters/next/app";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   const [queryClient] = useState(() => createQueryClient());
 
   return (
-    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    <QueryClientProvider client={queryClient}>
+      <NuqsAdapter>{children}</NuqsAdapter>
+    </QueryClientProvider>
   );
 }
