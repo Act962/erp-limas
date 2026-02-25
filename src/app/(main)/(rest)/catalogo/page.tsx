ae62a5db321fd3eea0e416e69d90fe3232f777b2
Arthur Fabricyo <arthur.fabricyo@gmail.com>
2026-02-25 13:27:45 -0300

    feat/page os catalog settings

--- src/app/(main)/(rest)/catalogo/page.tsx
+++ src/app/(main)/(rest)/catalogo/page.tsx
@@ -1,15 +1,4 @@
-import { getQueryClient, HydrateClient } from "@/lib/query/hydration";
 import { CatalogSettings } from "@/features/catalogo/components/catalog";
-import { orpc } from "@/lib/orpc";
 export default async function Page() {
-  const queryClient = getQueryClient();
-
-  await queryClient.prefetchQuery(orpc.catalogSettings.list.queryOptions());
-  return (
-    <>
-      <HydrateClient client={queryClient}>
-        <CatalogSettings />
-      </HydrateClient>
-    </>
-  );
+  return <CatalogSettings />;
 }
